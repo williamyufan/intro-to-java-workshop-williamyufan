7edfce2dcc8d016a25834e66d1c39e01dc347c6d
Sofisa <league@WTS7.attlocal.net>
2018-03-13 19:57:24 -0700

    day2

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -7,7 +7,31 @@ public class MyFirstJavaProgram {
 	
 	public static void main(String[] args) {
 		
-		// START HERE
+	Robot bobthebuilder = new Robot();
+	bobthebuilder.miniaturize();
+	bobthebuilder.sparkle();
+	bobthebuilder.setRandomPenColor();
+	bobthebuilder.penDown();
+	bobthebuilder.setSpeed(1000);
+	bobthebuilder.setAngle(220);
+	bobthebuilder.move(87);
+	bobthebuilder.turn(44);
+	bobthebuilder.move(58);
+	bobthebuilder.turn(140);
+	bobthebuilder.move(190);
+	bobthebuilder.turn(45);
+	bobthebuilder.move(170);
+	bobthebuilder.turn(135);
+	bobthebuilder.move(90);
+	for(int i=0; i<4; i++) {
+		bobthebuilder.move(90);
+		bobthebuilder.turn(90);
+	}
+	
+	
+	
+	
+	
 	
 		
 		
