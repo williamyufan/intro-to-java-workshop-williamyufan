a394325918c3922debe703035f4dfbaa112540c8
League <league@WTS8.attlocal.net>
2018-03-15 19:58:05 -0700

    day 4

--- src/section3/WorldDomination.java
+++ src/section3/WorldDomination.java
@@ -3,14 +3,20 @@ package section3;
 import javax.swing.JOptionPane;
 
 public class WorldDomination {
-	
+
 	public static void main(String[] args) {
-		// 1. Ask the user if they know how to write code.
+		String input = JOptionPane.showInputDialog("bob");
+		if (input.equalsIgnoreCase("bob")) {
 
-		// 2. If they say "yes", tell them they will rule the world.
+			JOptionPane.showMessageDialog(null, "you have a name");
+		}
 
-		// 3. Otherwise, wish them good luck washing dishes.
+		else {
+			JOptionPane.showMessageDialog(null, "your name is awful");
 
+		}
 	}
 }
+// 2. If they say "yes", tell them they will rule the world.
 
+// 3. Otherwise, wish them good luck washing dishes.
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,22 +1,74 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
-	
+
 	public static void main(String[] args) {
+
+		int scor = 0;
+
+		String input = JOptionPane.showInputDialog("Q: What's 190+198?");
 		
-		// 1.  Create a variable to hold the user's score 
+		if (input.equalsIgnoreCase("")) {
+			 									
+input =	JOptionPane.showInputDialog(null, "Q: 190+198. Are you sure you want to Submit?");
+		}
 		
-		// 2.  Ask the user a question 
+		if (input.equalsIgnoreCase("388")) {
+
+			JOptionPane.showMessageDialog(null, "You are CORRECT ");
+
+			scor = scor + 1;
+
+			JOptionPane.showMessageDialog(null, "You Score IS " + scor);
+		} else {
+			JOptionPane.showMessageDialog(null, "You are INCORRECT");
+		}
+
+		String input1 = JOptionPane.showInputDialog("What's 190+1978?");
 		
-		// 3.  Use an if statement to check if their answer is correct
+		if (input.equalsIgnoreCase("")) {
+			
+			input =	JOptionPane.showInputDialog(null, "Q: 190+1978. Are you sure you want to Submit?");
+					}
 		
-		// 4.  if the user's answer is correct
 		
-		// -- add one to their score 
+		if (input1.equalsIgnoreCase("2168")) {
+			
+
+			JOptionPane.showMessageDialog(null, "You are CORRECT ");
+
+			scor = scor + 1;
+			
+			JOptionPane.showMessageDialog(null, "You Score IS " + scor);
+		}
+
 		
-		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
+		 else {
+			JOptionPane.showMessageDialog(null, "You are INCORRECT");
+		}
+
+		String input2 = JOptionPane.showInputDialog("What's 1980.975+188.795?");
 		
-		// 6.  After all the questions have been asked, print the user's score 
+		if (input.equalsIgnoreCase("")) {
+			
+			input =	JOptionPane.showInputDialog(null, "Q: 190+1978. Are you sure you want to Submit?");
+					}
 		
+		if (input1.equalsIgnoreCase("2169.77")) {
+			
+
+			JOptionPane.showMessageDialog(null, "You are CORRECT ");
+
+			scor = scor + 1;
+
+		}
+
+		else {
+			JOptionPane.showMessageDialog(null, "You are INCORRECT");
+		}
+
+		JOptionPane.showMessageDialog(null, "You Score IS " + scor);
 	}
 }
